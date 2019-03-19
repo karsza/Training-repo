c81a0acbe064ca64312430e518890df2ac70e15b
Karolina Szatanik <karolina.szatanik1@gmail.com>
2019-03-19 20:17:15 +0100

    start of Binary tree

---
+++ BinaryTree/src/main/java/Node.java
@@ -0,0 +1,18 @@
+public class Node {
+
+    Node left;
+    Node right;
+    int value;
+
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public void add(int data){
+//        if(this.value == null)
+
+
+
+    }
+}
