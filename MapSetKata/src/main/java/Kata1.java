95da0ca7a114a74c7b96dc2ccff66077a6272103
Karolina Szatanik <karolina.szatanik1@gmail.com>
2019-05-10 11:29:59 +0200

    final Kata1 + test

---
+++ MapSetKata/src/main/java/Kata1.java
@@ -0,0 +1,22 @@
+public class Kata1 {
+    public static String longestConsec(String[] strarr, int k) {
+        String max="";
+        String temp;
+
+        for (int i=0;i<strarr.length;i++){
+            temp="";
+            for (int j=i;j<strarr.length&&j<i+k;j++)
+                temp=temp+strarr[j];
+            if (temp.length()>max.length()){
+                max=temp;
+            }
+            if (k>strarr.length){
+                max = "";
+            }
+            if (k==0){
+                max = "";
+            }
+        }
+        return max;
+    }
+}
