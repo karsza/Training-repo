2e1bedc5e23900ac67ae01b77d852008b3bfbcad
Karolina <Karolina@example.com>
2019-09-03 14:37:38 +0200

    kata+test added

---
+++ MapSetKata/src/main/java/Kata10.java
@@ -0,0 +1,5 @@
+//Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different
+// items in the array that, when added together, give the target value. The indices of these items should then be returned in
+// a tuple like so: (index1, index2).
+public class Kata10 {
+}
