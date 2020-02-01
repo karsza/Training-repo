4bffccc40dcb61a6ac3a1ec7379f4c6d9856ad0a
Karolina <Karolina@example.com>
2020-02-01 21:06:25 +0100

    more features added

--- SimplePlane1/src/index.js
+++ SimplePlane1/src/index.js
@@ -1,1 +1,10 @@
 import style from "./css/index.scss";
+$('.openmodale').click(function(e) {
+    e.preventDefault();
+    $('.modale').addClass('opened');
+});
+$('.closemodale').click(function(e) {
+    e.preventDefault();
+    $('.modale').removeClass('opened');
+});
+
